ce4463a8bf4250f499b262599a5c5ebd7130978d
Saurabh <saurabh.sahu@luezoid.com>
2020-04-29 16:59:32 +0530

    gl steps fix

--- Modules/Hr/Repositories/GradeLevelRepository.php
+++ Modules/Hr/Repositories/GradeLevelRepository.php
@@ -10,13 +10,4 @@ use Modules\Hr\Models\GradeLevel;
 class GradeLevelRepository extends EloquentBaseRepository
 {
     public $model = GradeLevel::class;
-
-    public function create($data)
-    {
-        if (!isset($data['data']['name'])) {
-            $currentGradeLevelCount = GradeLevel::where('salary_scale_id', $data['data']['salary_scale_id'])->count() +1;
-            $data['data']['name'] = 'Level-'.str_pad($currentGradeLevelCount, 2, '0', STR_PAD_LEFT);
-        }
-        return parent::create($data);
-    }
 }
