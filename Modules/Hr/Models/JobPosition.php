b8cff010e6a7384de547a020e35f18c82eb81f17
Saurabh <saurabh.sahu@luezoid.com>
2020-05-15 16:08:23 +0530

    job postion api

--- Modules/Hr/Models/JobPosition.php
+++ Modules/Hr/Models/JobPosition.php
@@ -12,7 +12,7 @@ use Reliese\Database\Eloquent\Model as Eloquent;
 
 /**
  * Class JobPosition
- * 
+ *
  * @property int $id
  * @property int $parent_id
  * @property int $department_id
@@ -35,7 +35,7 @@ use Reliese\Database\Eloquent\Model as Eloquent;
  * @property string $deleted_at
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
- * 
+ *
  * @property \Modules\Hr\Models\Department $department
  * @property \Modules\Hr\Models\Designation $designation
  * @property \Modules\Hr\Models\SalaryScale $hr_salary_scale
@@ -47,67 +47,67 @@ use Reliese\Database\Eloquent\Model as Eloquent;
  */
 class JobPosition extends Eloquent
 {
-	use \Illuminate\Database\Eloquent\SoftDeletes;
+    use \Illuminate\Database\Eloquent\SoftDeletes;
 
     protected $table = "hr_job_positions";
-	protected $casts = [
-		'parent_id' => 'int',
-		'department_id' => 'int',
-		'designation_id' => 'int',
-		'salary_scale_id' => 'int',
-		'grade_level_id' => 'int',
-		'grade_level_step_id' => 'int',
-		'skill_id' => 'int',
+    protected $casts = [
+        'parent_id' => 'int',
+        'department_id' => 'int',
+        'designation_id' => 'int',
+        'salary_scale_id' => 'int',
+        'grade_level_id' => 'int',
+        'grade_level_step_id' => 'int',
+        'skill_id' => 'int',
         'is_child_enabled' => 'bool',
-		'is_approved_position' => 'bool',
-		'is_active' => 'bool'
-	];
+        'is_approved_position' => 'bool',
+        'is_active' => 'bool'
+    ];
 
-	protected $fillable = [
-	    'name',
-	    'parent_id',
-		'department_id',
-		'designation_id',
+    protected $fillable = [
+        'name',
+        'parent_id',
+        'department_id',
+        'designation_id',
         'salary_scale_id',
         'grade_level_id',
-		'grade_level_step_id',
+        'grade_level_step_id',
         'is_child_enabled',
-		'skill_id',
-		'cost_center',
-		'job_family',
-		'is_approved_position',
-		'is_active',
-		'activities',
-		'competences',
-		'job_description_summary',
-		'experience',
-		'education'
-	];
+        'skill_id',
+        'cost_center',
+        'job_family',
+        'is_approved_position',
+        'is_active',
+        'activities',
+        'competences',
+        'job_description_summary',
+        'experience',
+        'education'
+    ];
 
-	public function department()
-	{
-		return $this->belongsTo(AdminSegment::class);
-	}
+    public function department()
+    {
+        return $this->belongsTo(AdminSegment::class);
+    }
 
-	public function designation()
-	{
-		return $this->belongsTo(\Modules\Hr\Models\Designation::class);
-	}
+    public function designation()
+    {
+        return $this->belongsTo(\Modules\Hr\Models\Designation::class);
+    }
 
     public function hr_salary_scale()
     {
         return $this->belongsTo(\Modules\Hr\Models\SalaryScale::class, 'salary_scale_id');
     }
 
-	public function hr_grade_level()
+    public function hr_grade_level()
     {
         return $this->belongsTo(\Modules\Hr\Models\GradeLevel::class, 'grade_level_id');
     }
 
-	public function grade_level_step()
-	{
-		return $this->belongsTo(\Modules\Hr\Models\GradeLevelStep::class);
-	}
+    public function grade_level_step()
+    {
+        return $this->belongsTo(\Modules\Hr\Models\GradeLevelStep::class);
+    }
 
     public function job_positions()
     {
@@ -119,14 +119,14 @@ class JobPosition extends Eloquent
         return $this->belongsTo(\Modules\Hr\Models\JobPosition::class, 'parent_id');
     }
 
-	public function skill()
-	{
-		return $this->belongsTo(\Modules\Hr\Models\Skill::class);
-	}
+    public function skill()
+    {
+        return $this->belongsTo(\Modules\Hr\Models\Skill::class);
+    }
 
     public function sub_categories()
     {
-        return $this->children()->with('sub_categories');
+        return $this->children()->with('department', 'sub_categories');
     }
 
     public function children()
