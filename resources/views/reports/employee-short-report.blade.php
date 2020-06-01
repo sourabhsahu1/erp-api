6ba521fa5764e05bbf724ea08ceb4fb2df034eb8
Saurabh <saurabh.sahu@luezoid.com>
2020-06-01 21:08:33 +0530

    html changes in short report

--- resources/views/reports/employee-short-report.blade.php
+++ resources/views/reports/employee-short-report.blade.php
@@ -1,28 +1,243 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Adapt Table</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css"  href="../css/bootstrap.min.css">
+<html>
+<head>
+    <title>Staff</title>
+    <style type="text/css">
+        * {
+            font-family: 'Noto Sans', sans-serif !important;
+        }
 
-        <style>
-            th{
-                font-size: 14px;
-            }
-            .sec-heading{
-                font-size: 17px;
-                font-weight: 400;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="row mt-5">
+        table td {
+            padding: 18px;
+        }
 
-                <div>short report</div>
+        table th {
+            padding: 18px;
+        }
 
+        .main-container {
+            box-shadow: 0 0 1px 2px #000;
+            padding: 50px;
+            border-radius: 10px;
+        }
+
+        .d-flex {
+            display: flex !important
+        }
+
+        .staff-details {
+            width: 45%
+        }
+
+        .staff-details-table {
+            width: 100%;
+            font-size: 24px;
+            border: 1px solid #e0e0e0;
+        }
+
+        .staff-details-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .staff-header {
+            background-color: #020a30;
+            color: white
+        }
+
+        .img-citizenship-details {
+            margin-left: 10%;
+            width: 45%;
+            font-size: 24px;
+        }
+
+        .citizenship-details {
+            width: 100%
+        }
+
+        .citizenship-details-table {
+            width: 100%;
+        }
+
+        .citizenship-details-header {
+            background-color: #020a30;
+            color: white
+        }
+
+        .citizenship-details-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .user-img {
+            box-shadow: 0 0 5px 3px #e0e0e0 !important;
+            width: 80%;
+            height: 433px !important;
+            margin-left: 10%;
+            margin-bottom: 20px;
+        }
+
+        .contact-address-details {
+            width: 100%;
+        }
+
+        .contact-address-table {
+            width: 100%;
+            margin-top: 70px;
+            font-size: 24px;
+            border: 1px solid #e0e0e0;
+        }
+
+        .contact-address-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .contact-address-header {
+            background-color: #020a30;
+            color: white
+        }
+    </style>
+</head>
+<body>
+<div class="main-container">
+    <div class="d-flex">
+        <div class="staff-details">
+            <table class="staff-details-table">
+                <thead class="staff-header">
+                <tr>
+                    <th colspan="2">Staff Details</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Staff ID:</td>
+                    <td>1234</td>
+                </tr>
+                <tr>
+                    <td>Last Name:</td>
+                    <td>Mishra</td>
+                </tr>
+                <tr>
+                    <td>Other Names:</td>
+                    <td>Abhishek</td>
+                </tr>
+                <tr>
+                    <td>Marital Status:</td>
+                    <td>S</td>
+                </tr>
+                <tr>
+                    <td>Religion:</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>Birth Date:</td>
+                    <td>05-Dec-94</td>
+                </tr>
+                <tr>
+                    <td>Department:</td>
+                    <td>05-Dec-94</td>
+                </tr>
+                <tr>
+                    <td>Designation:</td>
+                    <td>Front-End-Developer</td>
+                </tr>
+                <tr>
+                    <td>Date Employed:</td>
+                    <td>16-Feb-20</td>
+                </tr>
+                <tr>
+                    <td>Current App Date:</td>
+                    <td>16-Feb-20</td>
+                </tr>
+                <tr>
+                    <td>Location:</td>
+                    <td>Noida India</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="img-citizenship-details">
+            <div>
+                <img src="image.png" class="user-img" />
+            </div>
+            <div class="citizenship-details">
+                <table class="citizenship-details-table">
+                    <thead class="citizenship-details-header">
+                    <tr>
+                        <th colspan="2">Citizenship Details</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Country:</td>
+                        <td>India</td>
+                    </tr>
+                    <tr>
+                        <td>Region:</td>
+                        <td>#Error</td>
+                    </tr>
+                    <tr>
+                        <td>State:</td>
+                        <td>Abia</td>
+                    </tr>
+                    <tr>
+                        <td>LGA</td>
+                        <td>#Error</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </body>
+    </div>
+    <div class="contact-address-details">
+        <table class="contact-address-table">
+            <thead class="contact-address-header">
+            <tr>
+                <th colspan="2">Contact Address Details</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Address:</td>
+                <td>IND</td>
+            </tr>
+            <tr>
+                <td>Street:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>City:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Country:</td>
+                <td>IND</td>
+            </tr>
+            <tr>
+                <td>Region:</td>
+                <td>#Error</td>
+            </tr>
+            <tr>
+                <td>State:</td>
+                <td>Abia</td>
+            </tr>
+            <tr>
+                <td>LGA:</td>
+                <td>#Error</td>
+            </tr>
+            <tr>
+                <td>Sex:</td>
+                <td>M</td>
+            </tr>
+            <tr>
+                <td>Phone No.:</td>
+                <td>9545685952</td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>mishrawp@gmail.com</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</body>
 </html>
