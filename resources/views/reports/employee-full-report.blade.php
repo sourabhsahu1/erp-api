00077a9ba7c8ed882fff85957f850a73ffbce3e2
Saurabh <saurabh.sahu@luezoid.com>
2020-06-01 20:57:41 +0530

    html changes

--- resources/views/reports/employee-full-report.blade.php
+++ resources/views/reports/employee-full-report.blade.php
@@ -1,28 +1,334 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Adapt Table</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css"  href="../css/bootstrap.min.css">
-
-    <style>
-        th{
-            font-size: 14px;
+    <title>Profile Full</title>
+    <style type="text/css">
+        * {
+            font-family: 'Noto Sans', sans-serif !important;
         }
-        .sec-heading{
-            font-size: 17px;
-            font-weight: 400;
+
+        table td {
+            padding: 10px;
+        }
+
+        table th {
+            padding: 10px;
+        }
+
+        .info-table {
+            border: 1px solid #e0e0e0;
+            width: 38%;
+            font-size: 24px;
+        }
+
+        .info-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .image-table {
+            width: 20%;
+            font-size: 24px;
+            margin-left: 2%;
+            margin-right: 2%
+        }
+
+        .address-table {
+            border: 1px solid #e0e0e0;
+            width: 38%;
+            font-size: 24px;
+        }
+
+        .address-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .d-flex {
+            display: flex !important
+        }
+
+        .image-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .main-container {
+            box-shadow: 0 0 1px 2px #000;
+            padding: 50px;
+            border-radius: 10px;
+        }
+
+        .shadow {
+            box-shadow: 0 0 0 1px #e0e0e0;
+        }
+
+        .mr-38-per {
+            margin-right: 38%;
+        }
+
+        .mt--35px {
+            margin-top: -35px
+        }
+
+        .mt-15px {
+            margin-top: 15px;
+        }
+
+        .competencies-table {
+            border: 1px solid #e0e0e0;
+            width: 100%;
+            font-size: 24px;
+        }
+
+        .background-table {
+            border: 1px solid #e0e0e0;
+            width: 94%;
+            font-size: 24px;
+            margin-left: 15%;
+        }
+
+        .background-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .text-center{
+            text-align: center;
+        }
+
+        .user-img {
+            box-shadow: 0 0 5px 3px #e0e0e0 !important;
+            margin-left: 15%;
         }
     </style>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row mt-5">
-
-        <div>full report</div>
-
-
+<div class="main-container">
+    <div class="d-flex mt--35px">
+        <h2 class="mr-38-per">Profile</h2>
+        <h2>Mishra Abhishek, Abia State, India</h2>
+    </div>
+    <div class="d-flex">
+        <table class="info-table">
+            <tbody>
+            <tr>
+                <td colspan="2">
+                    <b>Surname:</b> Mishra
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Other Names:</b> Abhishek
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>ID Number:</b> 1234
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Date of Birth:</b> 05-Dec-1994
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Martial Status:</b> Single
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <b>Date of 1st Appt:</b> 16-Feb-2020
+                </td>
+                <td>
+                    <b>Length of Service:</b> 0 Years
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Relatives (Staff) / Time of Service:</b>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <table class="image-table">
+            <tbody>
+            <tr>
+                <td>
+                    <img src="image.png" class="user-img" />
+                </td>
+            </tr>
+            <tr class="shadow">
+                <td>
+                    <b>Email:</b> mishrawp@gmail.com
+                </td>
+            </tr>
+            <tr class="shadow">
+                <td>
+                    <b>Phone No:</b> 9545685952
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <table class="address-table">
+            <tbody>
+            <tr>
+                <td colspan="3">
+                    <b>Sector/Sub-Sector/Division:</b> 2424
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Department/Section:</b> Operations
+                </td>
+                <td>
+                    <b>Geographic Location:</b> Noida, India
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <b>Position:</b> VP Sales
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <b>Supervisor:</b> 05-Dec-1994
+                </td>
+                <td>
+                    <b>Level No:</b> 1
+                </td>
+                <td>
+                    <b>Other Language:</b> 1
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <b>Degree (Master or Specialization):</b> Single
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex mt-15px">
+        <h3 class="mr-38-per">COMPETENCIES:</h3>
+        <h3>BACKGROUND:</h3>
+    </div>
+    <div class="d-flex">
+        <div style="width: 38%">
+            <div class="d-flex">
+                <table class="competencies-table">
+                    <tr>
+                        <td>
+                            2efc
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            defcc
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            wsdcef
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="d-flex mt-15px">
+                <h3 class="mr-38-per">ACTIVITIES OF POSITION:</h3>
+            </div>
+            <table class="competencies-table">
+                <tr>
+                    <td>
+                        2efc
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        defcc
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        wsdcef
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>
+                        2efc
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        defcc
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        wsdcef
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div style="width: 57%">
+            <table class="background-table">
+                <thead style="background-color: #020a30; color: white">
+                <tr>
+                    <th>
+                        S/N
+                    </th>
+                    <th>
+                        YEAR
+                    </th>
+                    <th>
+                        LEVEL
+                    </th>
+                    <th>
+                        POSITION
+                    </th>
+                    <th>
+                        LOCATION
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="text-center">
+                        1.
+                    </td>
+                    <td class="text-center">
+                        2/2020
+                    </td>
+                    <td class="text-center">
+                        Front End Developer
+                    </td>
+                    <td class="text-center">
+                        VP Sales
+                    </td>
+                    <td class="text-center">
+                        Noida, IND
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-center">
+                        2.
+                    </td>
+                    <td class="text-center">
+                        2/2020
+                    </td>
+                    <td class="text-center">
+                        Front End Developer
+                    </td>
+                    <td class="text-center">
+                        VP Sales
+                    </td>
+                    <td class="text-center">
+                        Noida, IND
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 </body>
