aa133933688e805a9710f61d54d31280c5db319d
Manoj Singh Rawat <manojrawat1502@gmail.com>
2020-07-23 11:48:48 +0530

    lifo logic added

--- Modules/Inventory/Repositories/InvoiceRepository.php
+++ Modules/Inventory/Repositories/InvoiceRepository.php
@@ -19,81 +19,7 @@ class InvoiceRepository extends EloquentBaseRepository
 {
     public $model = InvoiceDetail::class;
 
-    public function srvPurchase($data)
-    {
-        DB::beginTransaction();
-        try {
-            $invoiceDetail = parent::create($data);
-            $items = null;
-            $taxes = null;
-            foreach ($data['data']['items'] as $item) {
-                /** @var InvoiceItem $availableQuantity */
-                $availableQuantity = InvoiceItem::where('item_id', $item['item_id'])->orderBy('id', 'desc')->first();
 
-                $invoiceItem = InvoiceItem::create([
-                    'store_id' => $data['data']['store_id'],
-                    'item_id' => $item['item_id'],
-                    'invoice_id' => $invoiceDetail->id,
-                    'measurement_id' => $item['measurement_id'],
-                    'description' => $item['description'],
-                    'available_balance' => $item['quantity'] + (!is_null($availableQuantity) ? $availableQuantity->available_balance : 0),
-                    'unit_cost' => $item['unit_cost'],
-                    'quantity' => $item['quantity'],
-                    'created_at' => Carbon::now()->toDateString(),
-                    'updated_at' => Carbon::now()->toDateString()
-                ]);
-
-//                ItemsLifoCost::create([
-//                    'item_id' => $item['item_id'],
-//                    'invoice_item_id' => $invoiceItem->id,
-//                    'invoice_id' => $invoiceDetail->id,
-//                    'quantity' => $item['quantity'],
-//                    'price' => $item['unit_cost'],
-//                    'available_quantity' => $item['quantity'],
-//                    'type' => AppConstant::TYPE_IN,
-//
-//                ]);
-//                ItemsFifoCost::create([
-//                    'item_id' => $item['item_id'],
-//                    'invoice_item_id' => $invoiceItem->id,
-//                    'invoice_id' => $invoiceDetail->id,
-//                    'quantity' => $item['quantity'],
-//                    'price' => $item['unit_cost'],
-//                    'available_quantity' => $item['quantity'],
-//                    'type' => AppConstant::TYPE_IN,
-//
-//                ]);
-//
-//                ItemsAvgCost::create([
-//                    'item_id' => $item['item_id'],
-//                    'invoice_item_id' => $invoiceItem->id,
-//                    'invoice_id' => $invoiceDetail->id,
-//                    'quantity' => $item['quantity'],
-//                    'price' => $item['unit_cost'],
-//                    'available_quantity' => $item['quantity'],
-//                    'type' => AppConstant::TYPE_IN,
-//
-//                ]);
-
-                foreach ($item['taxes'] as $tax) {
-                   $itemTaxes =  InvoiceTax::create([
-                        'invoice_id' => $invoiceDetail->id,
-                        'item_id' => $item['item_id'],
-                        'tax' => $tax['tax'],
-                        'tax_id' => $tax['id']
-                    ]);
-
-                }
-            }
-
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
-        DB::commit();
-
-        return $invoiceDetail;
-    }
 
     public function srvReturn($data)
     {
@@ -144,7 +70,6 @@ class InvoiceRepository extends EloquentBaseRepository
 
         return $invoiceDetail;
     }
-
 
     public function fifo($item)
     {
@@ -214,74 +139,6 @@ class InvoiceRepository extends EloquentBaseRepository
             }
     }
 
-    public function lifo($item)
-    {
-
-        //all items that were out
-//        foreach ($items as $item) {
-
-            //items that are available
-            $fifoItems = ItemsLifoCost::where('item_id', $item['item_id'])
-                ->where('quantity', '>', 0)
-                ->where('is_active', true)
-                ->where('type', AppConstant::TYPE_IN)
-                ->orderby('id', 'desc')
-                ->get();
-
-            //items list for fifo calculation
-            foreach ($fifoItems as $fifoItem) {
-                $quantity = $item['quantity'];
-                $item['quantity'] = $fifoItem->quantity - $item['quantity'];
-                if ($item['quantity'] == 0) {
-                    $update = ItemsLifoCost::where('id', $fifoItem->id)
-                        ->update([
-                            'available_quantity' => 0,
-                            'is_active' => false
-                        ]);
-                    $update = ItemsLifoCost::create([
-                        'item_id' => $item['item_id'],
-                        'invoice_id' => $item['invoice_id'],
-                        'quantity' => $quantity,
-                        'available_quantity' => $item['quantity'],
-                        'price' => $fifoItem->price,
-                        'type' => AppConstant::TYPE_OUT,
-                        'is_active' => false,
-                        'fifo_cost' => $fifoItem->price,
-                        'selling_price' => $item['selling_price']
-                    ]);
-                    break;
-                }
-                if ($item['quantity'] > 0) {
-                    $update = ItemsLifoCost::where('id', $fifoItem->id)
-                        ->update([
-                            'available_quantity' => $item['quantity']
-                        ]);
-                    $update = ItemsLifoCost::create([
-                        'item_id' => $item['item_id'],
-                        'invoice_id' => $item['invoice_id'],
-                        'quantity' => $quantity,
-                        'available_quantity' => $item['quantity'],
-                        'price' => $fifoItem->price,
-                        'type' => AppConstant::TYPE_OUT,
-                        'is_active' => false,
-                        'fifo_cost' => $fifoItem->price,
-                        'selling_price' => $item['selling_price']
-                    ]);
-                    break;
-                }
-                if ($item['quantity'] < 0) {
-                    $item['quantity'] = $item['quantity'] * (-1);
-                    $update = ItemsLifoCost::where('id', $fifoItem->id)
-                        ->update([
-                            'available_quantity' => 0,
-                            'is_active' => false
-                        ]);
-
-                }
-            }
-
-    }
-
     public function avg($item)
     {
         //all items that were out
@@ -335,63 +192,6 @@ class InvoiceRepository extends EloquentBaseRepository
             }
 
             InvoiceItem::insert($data['items']);
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
-        DB::commit();
-        return $invoiceDetail;
-    }
-
-    public function salesOutwards($data)
-    {
-
-        DB::beginTransaction();
-
-        try {
-            $invoiceDetail = parent::create($data);
-            $items = null;
-            foreach ($data['data']['items'] as $item) {
-                /** @var InvoiceItem $availableQuantity */
-                $availableQuantity = InvoiceItem::where('item_id', $item['item_id'])->orderBy('id', 'desc')->first();
-                $items = [
-                    'store_id' => $data['data']['store_id'],
-                    'item_id' => $item['item_id'],
-                    'invoice_id' => $invoiceDetail->id,
-                    'measurement_id' => $item['measurement_id'],
-                    'description' => $item['description'],
-                    'selling_price' => $item['selling_price'],
-                    'available_balance' => $availableQuantity->available_balance - $item['quantity'],
-                    'quantity' => $item['quantity'],
-                    'created_at' => Carbon::now()->toDateString(),
-                    'updated_at' => Carbon::now()->toDateString()
-                ];
-
-
-                foreach ($item['taxes'] as $tax) {
-                    $taxes = [
-                        'invoice_id' => $invoiceDetail->id,
-                        'item_id' => $item['item_id'],
-                        'tax' => $tax['tax'],
-                        'tax_id' => $tax['id']
-                    ];
-                    $data['taxes'][] = $taxes;
-                }
-                $data['items'][] = $items;
-
-
-//                $data[''] =
-                //fifo
-
-
-                //lifo
-
-
-                //avg
-
-            }
-            InvoiceItem::insert($data['items']);
-            InvoiceTax::insert($data['taxes']);
         } catch (\Exception $e) {
             DB::rollBack();
             throw $e;
@@ -742,5 +542,219 @@ class InvoiceRepository extends EloquentBaseRepository
                 ->where('inventory_invoice_items.store_id', '=', $params['inputs']['store_id']);
         }
         return $query->get();
+    }
+
+    // data in
+    public function srvPurchase($data)
+    {
+        DB::beginTransaction();
+        try {
+            $invoiceDetail = parent::create($data);
+            $items = null;
+            $taxes = null;
+            foreach ($data['data']['items'] as $item) {
+                /** @var InvoiceItem $availableQuantity */
+                $availableQuantity = InvoiceItem::where('item_id', $item['item_id'])->orderBy('id', 'desc')->first();
+
+                $invoiceItem = InvoiceItem::create([
+                    'store_id' => $data['data']['store_id'],
+                    'item_id' => $item['item_id'],
+                    'invoice_id' => $invoiceDetail->id,
+                    'measurement_id' => $item['measurement_id'],
+                    'description' => $item['description'],
+                    'available_balance' => $item['quantity'] + (!is_null($availableQuantity) ? $availableQuantity->available_balance : 0),
+                    'unit_cost' => $item['unit_cost'],
+                    'quantity' => $item['quantity'],
+                    'created_at' => Carbon::now()->toDateString(),
+                    'updated_at' => Carbon::now()->toDateString()
+                ]);
+//                ItemsLifoCost::create([
+//                    'item_id' => $item['item_id'],
+//                    'invoice_item_id' => $invoiceItem->id,
+//                    'invoice_id' => $invoiceDetail->id,
+//                    'quantity' => $item['quantity'],
+//                    'price' => $item['unit_cost'],
+//                    'available_quantity' => $item['quantity'],
+//                    'type' => AppConstant::TYPE_IN,
+//
+//                ]);
+//                ItemsFifoCost::create([
+//                    'item_id' => $item['item_id'],
+//                    'invoice_item_id' => $invoiceItem->id,
+//                    'invoice_id' => $invoiceDetail->id,
+//                    'quantity' => $item['quantity'],
+//                    'price' => $item['unit_cost'],
+//                    'available_quantity' => $item['quantity'],
+//                    'type' => AppConstant::TYPE_IN,
+//
+//                ]);
+//
+//                ItemsAvgCost::create([
+//                    'item_id' => $item['item_id'],
+//                    'invoice_item_id' => $invoiceItem->id,
+//                    'invoice_id' => $invoiceDetail->id,
+//                    'quantity' => $item['quantity'],
+//                    'price' => $item['unit_cost'],
+//                    'available_quantity' => $item['quantity'],
+//                    'type' => AppConstant::TYPE_IN,
+//
+//                ]);
+
+                foreach ($item['taxes'] as $tax) {
+                    $itemTaxes =  InvoiceTax::create([
+                        'invoice_id' => $invoiceDetail->id,
+                        'item_id' => $item['item_id'],
+                        'tax' => $tax['tax'],
+                        'tax_id' => $tax['id']
+                    ]);
+
+                }
+            }
+            $this->lifo($invoiceDetail->id, AppConstant::TYPE_IN);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+        DB::commit();
+
+        return $invoiceDetail;
+    }
+
+    // data out
+    public function salesOutwards($data)
+    {
+
+        DB::beginTransaction();
+
+        try {
+            $invoiceDetail = parent::create($data);
+            $items = null;
+            $invoiceDbItems = [];
+            foreach ($data['data']['items'] as $item) {
+                /** @var InvoiceItem $availableQuantity */
+                $availableQuantity = InvoiceItem::where('item_id', $item['item_id'])->orderBy('id', 'desc')->first();
+
+                $items = [
+                    'store_id' => $data['data']['store_id'],
+                    'item_id' => $item['item_id'],
+                    'invoice_id' => $invoiceDetail->id,
+                    'measurement_id' => $item['measurement_id'],
+                    'description' => $item['description'],
+                    'selling_price' => $item['selling_price'],
+                    'available_balance' => $availableQuantity->available_balance - $item['quantity'],
+                    'quantity' => $item['quantity'],
+                    'created_at' => Carbon::now()->toDateString(),
+                    'updated_at' => Carbon::now()->toDateString()
+                ];
+
+
+                foreach ($item['taxes'] as $tax) {
+                    $taxes = [
+                        'invoice_id' => $invoiceDetail->id,
+                        'item_id' => $item['item_id'],
+                        'tax' => $tax['tax'],
+                        'tax_id' => $tax['id']
+                    ];
+                    $data['taxes'][] = $taxes;
+                }
+                $data['items'][] = $items;
+
+
+//                $data[''] =
+                //fifo
+
+
+                //lifo
+
+
+                //avg
+
+            }
+            InvoiceItem::insert($data['items']);
+            InvoiceTax::insert($data['taxes']);
+            $this->lifo($invoiceDetail->id, AppConstant::TYPE_OUT);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+        DB::commit();
+        return $invoiceDetail;
+    }
+
+    // todo change this
+    public function lifo($invoiceId, $type)
+    {
+        $items = InvoiceItem::where('invoice_id', $invoiceId)->get()->toArray();
+
+        foreach ($items as $item) {
+            $lastBatch = ItemsLifoCost::where('is_active', true)->get()->toArray();
+            $dataToBeInserted = [];
+
+            if ($type === AppConstant::TYPE_IN) {
+                foreach ($lastBatch as $batch) {
+                    unset($batch['id']);
+                    unset($batch['created_at']);
+                    unset($batch['updated_at']);
+                    $batch['invoice_item_id'] = $item['id'];
+                    $batch['created_at'] = Carbon::now();
+                    $batch['updated_at'] = Carbon::now();
+                    $dataToBeInserted[] = $batch;
+                }
+                $dataToBeInserted[] = [
+                    'item_id' => $item['item_id'],
+                    'invoice_item_id' => $item['id'],
+                    'invoice_id' => $item['invoice_id'],
+                    'quantity' => $item['quantity'],
+                    'available_quantity' => $item['quantity'],
+                    'price' => $item['unit_cost'],
+                    'type' => $type,
+                    'is_active' => true,
+                    'lifo_cost' => $item['unit_cost'],
+                    'selling_price' => $item['unit_cost'],
+                    'created_at'=> Carbon::now(),
+                    'updated_at'=> Carbon::now()
+                ];
+            } else {
+                $activeItems = ItemsLifoCost::where('is_active', true)->where('item_id', $item['item_id'])->orderBy('id', 'DESC')->get()->toArray();
+                $deletedIds = [];
+                $activeItemQtyMap = [];
+                $activeQty = $item['quantity'];
+                foreach ($activeItems as $activeItem) {
+                    if ($activeQty >= $activeItem['quantity']) {
+                        $deletedIds[] = $activeItem['id'];
+                        $activeQty -= $activeItem['quantity'];
+                        if ($activeQty < 0) {
+                            break;
+                        }
+                    } else {
+                        $activeItemQtyMap[$activeItem['id']] = $activeItem['quantity'] - $activeQty;
+                        break;
+                    }
+                }
+
+                foreach ($lastBatch as &$batch) {
+                    if (array_search($batch['id'], $deletedIds) !== false) {
+                        continue;
+                    }
+                    if (isset($activeItemQtyMap[$batch['id']])) {
+                        $batch['quantity'] = $activeItemQtyMap[$batch['id']];
+                    }
+                    unset($batch['id']);
+                    unset($batch['created_at']);
+                    unset($batch['updated_at']);
+                    $batch['invoice_item_id'] = $item['id'];
+                    $batch['created_at'] = Carbon::now();
+                    $batch['updated_at'] = Carbon::now();
+                    $dataToBeInserted[] = $batch;
+                }
+            }
+            ItemsLifoCost::where('is_active', true)->update(['is_active' => false]);
+            ItemsLifoCost::insert($dataToBeInserted);
+            /*ItemsLifoCost::create([
+
+            ]);*/
+        }
+
     }
 }
