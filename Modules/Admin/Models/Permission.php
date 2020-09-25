fef89dd2d351e2feae7d401660badcfc7f5a6d59
Saurabh <saurabh.sahu@luezoid.com>
2020-09-25 11:31:58 +0530

    permission and role permission migration and get apis

---
+++ Modules/Admin/Models/Permission.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Thu, 24 Sep 2020 19:03:25 +0000.
+ */
+
+namespace Modules\Admin\Models;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Permission
+ * 
+ * @property int $id
+ * @property string $name
+ * @property string $entity_name
+ * @property string $deleted_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * 
+ * @property \Illuminate\Database\Eloquent\Collection $roles
+ *
+ * @package Modules\Admin\Models
+ */
+class Permission extends Eloquent
+{
+	use \Illuminate\Database\Eloquent\SoftDeletes;
+
+	protected $fillable = [
+		'name',
+		'entity_name'
+	];
+
+	public function roles()
+	{
+		return $this->belongsToMany(\Modules\Hr\Models\Role::class, 'role_permissions')
+					->withPivot('id', 'deleted_at')
+					->withTimestamps();
+	}
+}
--- Modules/Admin/Repositories/RoleRepository.php
+++ Modules/Admin/Repositories/RoleRepository.php
@@ -4,11 +4,30 @@
 namespace Modules\Admin\Repositories;
 
 
+
 use Luezoid\Laravelcore\Repositories\EloquentBaseRepository;
+use Modules\Admin\Models\Permission;
 use Modules\Hr\Models\Role;
 
 class RoleRepository extends EloquentBaseRepository
 {
 
     public $model = Role::class;
+
+    public function assignPermission($data)
+    {
+        $roleId = array_get($data['data'], 'id');
+        $permissionIds = array_get($data['data'], 'permission_ids');
+        $role = $this->model::find($roleId);
+        $role->touch();
+        $role->permissions()->sync($permissionIds);
+        return Role::with('permissions')->find($roleId);
+    }
+
+
+    public function getPermissions($data)
+    {
+        $roleId = array_get($data['inputs'], 'id');
+        return Permission::join('role_permissions', 'role_permissions.permission_id', '=', 'permissions.id')->where('role_permissions.role_id', $roleId)->select(['permissions.*'])->get();
+    }
 }
