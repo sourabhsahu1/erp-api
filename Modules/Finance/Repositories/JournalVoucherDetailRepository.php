7ed6585f4d880317362dd4c99f69a6033a0a54b3
Saurabh <saurabh.sahu@luezoid.com>
2020-11-06 16:24:59 +0530

    jv detail update fixed

--- Modules/Finance/Repositories/JournalVoucherDetailRepository.php
+++ Modules/Finance/Repositories/JournalVoucherDetailRepository.php
@@ -28,13 +28,16 @@ class JournalVoucherDetailRepository extends EloquentBaseRepository
 
     public function update($data)
     {
+        $data['data']['journal_voucher_id'] = $data['data']['id'];
+        $jvDetail = JournalVoucherDetail::where('journal_voucher_id', $data['data']['id'])
+            ->where('id', $data['data']['detail_id'])
+            ->first();
 
-        $jvDetail = JournalVoucherDetail::where('journal_voucher_id', $data['data']['id'])->where('id', $data['data']['detail_id'])->first();
+        $jvDetailCheck = JournalVoucherDetail::where('journal_voucher_id', $data['data']['journal_voucher_id'])
+            ->where('economic_segment_id', $data['data']['economic_segment_id'])
+            ->first();
 
-        $jvDetail = JournalVoucherDetail::where('journal_voucher_id', $data['data']['journal_voucher_id'])
-            ->where('economic_segment_id', $data['data']['economic_segment_id'])->first();
-
-        if ($jvDetail) {
+        if ($jvDetailCheck) {
             throw new AppException('Detail Line with this economic segment already exists');
         }
 
