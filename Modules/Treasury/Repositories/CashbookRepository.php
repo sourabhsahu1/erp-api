0126bd7b762408e7c9732cc38e8160adfd305bd4
Saurabh <saurabh.sahu@luezoid.com>
2020-11-12 17:44:23 +0530

    update cashbook api

--- Modules/Treasury/Repositories/CashbookRepository.php
+++ Modules/Treasury/Repositories/CashbookRepository.php
@@ -35,9 +35,40 @@ class CashbookRepository extends EloquentBaseRepository
         return Cashbook::with('cashbook_monthly_balances')->find($cashbook->id);
     }
 
+
+    public function update($data)
+    {
+
+        $cashbookMonthly = $data['data']['cashbook_monthly'];
+        unset($data['data']['cashbook_monthly']);
+
+        DB::beginTransaction();
+        try {
+            $cashbook = parent::update($data);
+
+            CashbookMonthlyBalance::where('cashbook_id', $data['id'])->delete();
+
+            foreach ($cashbookMonthly as $key => &$item) {
+                unset($item['id']);
+                $cashbookMonthly[$key]['cashbook_id'] = $cashbook->id;
+            }
+
+            CashbookMonthlyBalance::insert($cashbookMonthly);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+
+        return Cashbook::with('cashbook_monthly_balances')->find($data['id']);
+    }
+
+
     public function getCashbookTypes($params)
     {
         $this->model = CashbookType::class;
         return parent::getAll($params);
     }
+
+
 }
