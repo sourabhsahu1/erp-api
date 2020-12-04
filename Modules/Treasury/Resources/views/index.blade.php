c6ea97a34286c949ac5a1aeafa98275854cfdae4
Saurabh <saurabh.sahu@luezoid.com>
2020-12-04 22:57:24 +0530

    removed treasury

--- Modules/Treasury/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('treasury::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('treasury.name') !!}
-    </p>
-@endsection
