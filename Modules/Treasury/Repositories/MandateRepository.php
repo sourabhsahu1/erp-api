7307a78bb747bfd576a652c4c00acb565b4a5814
Saurabh <saurabh.sahu@luezoid.com>
2021-01-15 19:55:18 +0530

    retire pv to jv credit

--- Modules/Treasury/Repositories/MandateRepository.php
+++ Modules/Treasury/Repositories/MandateRepository.php
@@ -40,52 +40,48 @@ class MandateRepository extends EloquentBaseRepository
     public function create($data)
     {
 
-//        $latestMandate = Mandate::latest()->orderby('id', 'desc')->first();
-//        if (is_null($latestMandate)) {
-//            $data['data']['batch_number'] = 1;
-//            $data['data']['treasury_number'] = 1;
-//        } else {
-//            $data['data']['batch_number'] = $latestMandate->batch_number + 1;
-//            $data['data']['treasury_number'] = $latestMandate->treasury_number + 1;
-//        }
+        DB::beginTransaction();
+        try {
+
+            $data['data']['status'] = 'NEW';
+            $data['data']['prepared_by'] = $data['data']['user_id'];
+            $data['data']['prepared_date'] = Carbon::now()->toDateString();
+            $mandate = parent::create($data);
+
+            if (isset($data['data']['payment_vouchers'])) {
 
 
-        $data['data']['status'] = 'NEW';
-        $data['data']['prepared_by'] = $data['data']['user_id'];
-        $data['data']['prepared_date'] = Carbon::now()->toDateString();
-        $mandate = parent::create($data);
-
-        if (isset($data['data']['payment_vouchers'])) {
+                $data['data']['payment_vouchers'] = json_decode($data['data']['payment_vouchers'], true);
 
 
-            $data['data']['payment_vouchers'] = json_decode($data['data']['payment_vouchers'], true);
+                $paymentV = PaymentVoucher::whereIn('id', $data['data']['payment_vouchers'])
+                    ->whereIn('status', [
+                        AppConstant::VOUCHER_STATUS_AUDITED
+                    ])->get();
 
 
-
-            $paymentV = PaymentVoucher::whereIn('id', $data['data']['payment_vouchers'])
-                ->whereIn('status', [
-                    AppConstant::VOUCHER_STATUS_AUDITED
-                ])->get();
-
-
-
-            if ($paymentV->isEmpty()) {
-                throw new AppException('payment vouchers are not audited');
-            }
-
-            /** @var PaymentVoucher $payment */
-            foreach ($paymentV as $payment) {
-                if(strtotime($data['data']['value_date']) < strtotime($payment->value_date)) {
-                    throw new AppException('mandate date cannot be before than PV\'s value date');
+                if ($paymentV->isEmpty()) {
+                    throw new AppException('payment vouchers are not audited');
                 }
 
-            }
+                /** @var PaymentVoucher $payment */
+                foreach ($paymentV as $payment) {
+                    if (strtotime($data['data']['value_date']) < strtotime($payment->value_date)) {
+                        throw new AppException('mandate date cannot be before than PV\'s value date');
+                    }
 
-            $paymentVouchers = PaymentVoucher::whereIn('id', $data['data']['payment_vouchers'])
-                ->update([
-                    'mandate_id' => $mandate->id,
-                    'status' => AppConstant::VOUCHER_STATUS_ON_MANDATE
-                ]);
+                }
+
+                $paymentVouchers = PaymentVoucher::whereIn('id', $data['data']['payment_vouchers'])
+                    ->update([
+                        'mandate_id' => $mandate->id,
+                        'status' => AppConstant::VOUCHER_STATUS_ON_MANDATE
+                    ]);
+            }
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            throw  $exception;
         }
 
         return $mandate;
@@ -173,7 +169,7 @@ class MandateRepository extends EloquentBaseRepository
                         foreach ($paymentVouchers as $paymentVoucher) {
                             $currency = Currency::find($paymentVoucher->currency_id);
                             $jv = JournalVoucher::create([
-                                'source_app' => 'TREASURY',
+                                'source_app' => 'E-Voucher (Treasury)',
                                 'batch_number' => 1,
                                 'jv_value_date' => Carbon::now()->toDateTimeString(),
                                 'fund_segment_id' => $paymentVoucher->fund_segment_id,
@@ -194,6 +190,7 @@ class MandateRepository extends EloquentBaseRepository
                             /** @var PayeeVoucher $payee_voucher */
 
                             $jvD = null;
+                            $totalNetAmount = 0;
                             foreach ($paymentVoucher->payee_vouchers as $payee_voucher) {
                                 /** @var ScheduleEconomic $schedule_economic */
                                 foreach ($payee_voucher->schedule_economics as $schedule_economic) {
@@ -218,9 +215,35 @@ class MandateRepository extends EloquentBaseRepository
                                         'created_at' => Carbon::now()->toDateTimeString(),
                                         'updated_at' => Carbon::now()->toDateTimeString()
                                     ];
+
+
+                                    $totalNetAmount = $totalNetAmount + $schedule_economic->amount;
+
                                 }
                             }
 
+                            //entry for credit
+                            //todo credit updation pending in cashbook on respected ecnomic segment
+                            JournalVoucherDetail::create([
+                                'journal_voucher_id' => $jv->id,
+                                'currency' => $currency->code_currency,
+                                'x_rate_local' => $paymentVoucher->x_rate,
+                                'bank_x_rate_to_usd' => $paymentVoucher->official_x_rate,
+                                'account_name' => $paymentVoucher->deptal_id,
+                                'line_reference' => $paymentVoucher->deptal_id,
+                                'line_value' => $totalNetAmount,
+                                'admin_segment_id' => $paymentVoucher->admin_segment_id,
+                                'fund_segment_id' => $paymentVoucher->fund_segment_id,
+                                'economic_segment_id' => $paymentVoucher->economic_segment_id,
+                                'programme_segment_id' => $paymentVoucher->program_segment_id,
+                                'functional_segment_id' => $paymentVoucher->functional_segment_id,
+                                'geo_code_segment_id' => $paymentVoucher->geo_code_segment_id,
+                                'line_value_type' => 'CREDIT',
+                                'lv_line_value' => $totalNetAmount * $paymentVoucher->x_rate,
+                                'local_currency' => $companySetting->local_currency,
+                                'created_at' => Carbon::now()->toDateTimeString(),
+                                'updated_at' => Carbon::now()->toDateTimeString()
+                            ]);
                             JournalVoucherDetail::insert($jvD);
                         }
                     }
--- Modules/Treasury/Repositories/ReceiptVoucherRepository.php
+++ Modules/Treasury/Repositories/ReceiptVoucherRepository.php
@@ -5,8 +5,14 @@ namespace Modules\Treasury\Repositories;
 
 
 use App\Constants\AppConstant;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Luezoid\Laravelcore\Exceptions\AppException;
 use Luezoid\Laravelcore\Repositories\EloquentBaseRepository;
+use Modules\Admin\Models\CompanySetting;
+use Modules\Finance\Models\Currency;
+use Modules\Finance\Models\JournalVoucher;
+use Modules\Finance\Models\JournalVoucherDetail;
 use Modules\Treasury\Models\ReceiptPayee;
 use Modules\Treasury\Models\ReceiptScheduleEconomic;
 use Modules\Treasury\Models\ReceiptVoucher;
@@ -69,7 +75,7 @@ class ReceiptVoucherRepository extends EloquentBaseRepository
     {
         /** @var VoucherSourceUnit $vsu */
 
-            $vsu = VoucherSourceUnit::where('id', $params['inputs']['id'])->first();
+        $vsu = VoucherSourceUnit::where('id', $params['inputs']['id'])->first();
 
         if (is_null($vsu)) {
             throw new AppException('voucher source unit not exist');
@@ -126,12 +132,16 @@ class ReceiptVoucherRepository extends EloquentBaseRepository
     public function updateStatus($data)
     {
 
-        $pv = ReceiptVoucher::whereIn('id', $data['data']['receipt_voucher_ids']);
+        $rv = ReceiptVoucher::whereIn('id', $data['data']['receipt_voucher_ids']);
 
         foreach ($data['data']['receipt_voucher_ids'] as $receipt_voucher_id) {
 
-            $pv = ReceiptVoucher::where('id', $receipt_voucher_id)->first();
-            $payeeVoucherIds = ReceiptPayee::where('receipt_voucher_id', $pv->id)->pluck('id')->all();
+            $rv = ReceiptVoucher::where('id', $receipt_voucher_id)->first();
+
+            if (is_null($rv)) {
+                throw  new AppException('receipt voucher not exists');
+            }
+            $payeeVoucherIds = ReceiptPayee::where('receipt_voucher_id', $rv->id)->pluck('id')->all();
 
             if (is_null($payeeVoucherIds)) {
                 throw new AppException('Payee not added');
@@ -144,10 +154,107 @@ class ReceiptVoucherRepository extends EloquentBaseRepository
             }
         }
 
+        DB::beginTransaction();
+        try {
+            if (isset($data['data']['status'])) {
+                $retireVouchers = ReceiptVoucher::with(['receipt_payees.treasury_receipt_schedule_economics'])->whereIn('id', $data['data']['receipt_voucher_ids'])->get();
 
-        $pv->update([
-            'status' => $data['data']['status']
-        ]);
+                $companySetting = CompanySetting::find(1);
+                foreach ($retireVouchers as $retireVoucher) {
+
+                    $currencyId = $retireVoucher->cashbook->currency_id;
+                    $currency = Currency::find($currencyId);
+
+                    $jv = JournalVoucher::create([
+                        'source_app' => 'E-Voucher (Treasury)',
+                        'batch_number' => 1,
+                        'jv_value_date' => Carbon::now()->toDateTimeString(),
+                        'fund_segment_id' => $retireVoucher->fund_segment_id,
+                        'jv_reference' => $retireVoucher->source_department,
+                        'status' => AppConstant::VOUCHER_STATUS_NEW,
+                        'transaction_details' => $retireVoucher->payment_description,
+                        'prepared_value_date' => Carbon::now()->toDateTimeString(),
+                        'prepared_transaction_date' => Carbon::now()->toDateTimeString(),
+                        'checked_value_date' => null,
+                        'checked_transaction_date' => null,
+                        'posted_value_date' => null,
+                        'posted_transaction_date' => null,
+                        'prepared_user_id' => null,
+                        'checked_user_id' => null,
+                        'posted_user_id' => null
+                    ]);
+
+                    /** @var ReceiptPayee $receipt_payee */
+
+                    $jvD = null;
+                    $totalNetAmount = 0;
+                    foreach ($retireVoucher->receipt_payees as $receipt_payee) {
+                        /** @var ReceiptScheduleEconomic $schedule_economic */
+                        foreach ($receipt_payee->treasury_receipt_schedule_economics as $schedule_economic) {
+
+                            $jvD[] = [
+                                'journal_voucher_id' => $jv->id,
+                                'currency' => $currency->code_currency,
+                                'x_rate_local' => $retireVoucher->x_rate,
+                                'bank_x_rate_to_usd' => $retireVoucher->official_x_rate,
+                                'account_name' => $retireVoucher->deptal_id,
+                                'line_reference' => $retireVoucher->deptal_id,
+                                'line_value' => $schedule_economic->amount,
+                                'admin_segment_id' => $retireVoucher->admin_segment_id,
+                                'fund_segment_id' => $retireVoucher->fund_segment_id,
+                                'economic_segment_id' => $schedule_economic->economic_segment_id,
+                                'programme_segment_id' => $retireVoucher->program_segment_id,
+                                'functional_segment_id' => $retireVoucher->functional_segment_id,
+                                'geo_code_segment_id' => $retireVoucher->geo_code_segment_id,
+                                'line_value_type' => 'DEBIT',
+                                'lv_line_value' => $schedule_economic->amount * $retireVoucher->x_rate,
+                                'local_currency' => $companySetting->local_currency,
+                                'created_at' => Carbon::now()->toDateTimeString(),
+                                'updated_at' => Carbon::now()->toDateTimeString()
+                            ];
+
+                            $totalNetAmount = $totalNetAmount + $schedule_economic->amount;
+
+                        }
+                    }
+
+                    //entry for credit
+                    //todo credit updation pending in cashbook on respected economic segment
+                    JournalVoucherDetail::create([
+                        'journal_voucher_id' => $jv->id,
+                        'currency' => $currency->code_currency,
+                        'x_rate_local' => $retireVoucher->x_rate,
+                        'bank_x_rate_to_usd' => $retireVoucher->official_x_rate,
+                        'account_name' => $retireVoucher->deptal_id,
+                        'line_reference' => $retireVoucher->deptal_id,
+                        'line_value' => $totalNetAmount,
+                        'admin_segment_id' => $retireVoucher->admin_segment_id,
+                        'fund_segment_id' => $retireVoucher->fund_segment_id,
+                        'economic_segment_id' => $retireVoucher->economic_segment_id,
+                        'programme_segment_id' => $retireVoucher->program_segment_id,
+                        'functional_segment_id' => $retireVoucher->functional_segment_id,
+                        'geo_code_segment_id' => $retireVoucher->geo_code_segment_id,
+                        'line_value_type' => 'CREDIT',
+                        'lv_line_value' => $totalNetAmount * $retireVoucher->x_rate,
+                        'local_currency' => $companySetting->local_currency,
+                        'created_at' => Carbon::now()->toDateTimeString(),
+                        'updated_at' => Carbon::now()->toDateTimeString()
+                    ]);
+                    JournalVoucherDetail::insert($jvD);
+                }
+
+                $rv->update([
+                    'status' => $data['data']['status']
+                ]);
+            } else {
+                throw new AppException('Status is required');
+            }
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+
 
         return [
             'data' => 'Status Updated Successfully'
