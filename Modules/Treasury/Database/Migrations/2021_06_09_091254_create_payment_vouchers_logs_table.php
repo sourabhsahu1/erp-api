e47cbd04ba292f6d43ad00638875165e44432942
Saurabh <saurabh.sahu@luezoid.com>
2021-06-09 14:58:30 +0530

    migration

---
+++ Modules/Treasury/Database/Migrations/2021_06_09_091254_create_payment_vouchers_logs_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Constants\AppConstant;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentVouchersLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_vouchers_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('payment_voucher_id');
+            $table->enum('previous_status',[
+                AppConstant::VOUCHER_STATUS_CHECKED,
+                AppConstant::VOUCHER_STATUS_DRAFT,
+                AppConstant::VOUCHER_STATUS_APPROVED,
+                AppConstant::VOUCHER_STATUS_AUDITED,
+                AppConstant::VOUCHER_STATUS_BUDGET_CONTROL_VERIFIED,
+                AppConstant::VOUCHER_STATUS_CLOSED,
+                AppConstant::VOUCHER_STATUS_NEW,
+                AppConstant::VOUCHER_STATUS_ON_MANDATE,
+                AppConstant::VOUCHER_STATUS_PAID,
+                AppConstant::VOUCHER_STATUS_POSTED_TO_GL
+            ]);
+            $table->enum('current_status',[
+                AppConstant::VOUCHER_STATUS_CHECKED,
+                AppConstant::VOUCHER_STATUS_DRAFT,
+                AppConstant::VOUCHER_STATUS_APPROVED,
+                AppConstant::VOUCHER_STATUS_AUDITED,
+                AppConstant::VOUCHER_STATUS_BUDGET_CONTROL_VERIFIED,
+                AppConstant::VOUCHER_STATUS_CLOSED,
+                AppConstant::VOUCHER_STATUS_NEW,
+                AppConstant::VOUCHER_STATUS_ON_MANDATE,
+                AppConstant::VOUCHER_STATUS_PAID,
+                AppConstant::VOUCHER_STATUS_POSTED_TO_GL
+            ]);
+            $table->date('date');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('payment_voucher_id')->references('id')->on('treasury_payment_vouchers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_vouchers_logs');
+    }
+}
