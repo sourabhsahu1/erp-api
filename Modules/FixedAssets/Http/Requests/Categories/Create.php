5db5dc67701f953de5fbb42c67ef671dbd7cd224
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-12-21 21:03:06 +0530

    Fixed Asset category: remove validation

--- Modules/FixedAssets/Http/Requests/Categories/Create.php
+++ Modules/FixedAssets/Http/Requests/Categories/Create.php
@@ -10,17 +10,17 @@ class Create  extends BaseRequest
     {
         return [
             'title' => 'required',
-            'fixed_asset_acct_id' => 'required',
-            'accum_depr_acct_id' => 'required',
-            'depr_exps_acct_id' => 'required',
-            'asset_no_prefix_line' => 'required',
-            'asset_no_prefix_full' => 'required',
-            'next_asset_no' => 'sometimes',
-            'parent_id' => 'sometimes',
-            'ref_no_to_root_node' => 'sometimes',
-            'is_parent' => 'required',
-            'is_editable' => 'required',
-            'level_no' => 'required'
+//            'fixed_asset_acct_id' => 'required',
+//            'accum_depr_acct_id' => 'required',
+//            'depr_exps_acct_id' => 'required',
+//            'asset_no_prefix_line' => 'required',
+//            'asset_no_prefix_full' => 'required',
+//            'next_asset_no' => 'sometimes',
+//            'parent_id' => 'sometimes',
+//            'ref_no_to_root_node' => 'sometimes',
+//            'is_parent' => 'required',
+//            'is_editable' => 'required',
+//            'level_no' => 'required'
         ];
     }
 
