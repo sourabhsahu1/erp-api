1752fc2b5bf837e2eebd37fe622f195db7aae113
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-12-21 20:48:53 +0530

    Fixed Asset category: nullable

--- Modules/FixedAssets/Database/Migrations/2021_10_21_070017_fxa_categories.php
+++ Modules/FixedAssets/Database/Migrations/2021_10_21_070017_fxa_categories.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class FxaCategories extends Migration
 {
@@ -16,17 +16,17 @@ class FxaCategories extends Migration
         Schema::create('fxa_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->unsignedInteger('fixed_asset_acct_id');
-            $table->unsignedInteger('accum_depr_acct_id');
-            $table->unsignedInteger('depr_exps_acct_id');
-            $table->string('asset_no_prefix_line','10');
-            $table->string('asset_no_prefix_full','20');
+            $table->unsignedInteger('fixed_asset_acct_id')->nullable();
+            $table->unsignedInteger('accum_depr_acct_id')->nullable();
+            $table->unsignedInteger('depr_exps_acct_id')->nullable();
+            $table->string('asset_no_prefix_line', '10')->nullable();
+            $table->string('asset_no_prefix_full', '20')->nullable();
             $table->unsignedBigInteger('next_asset_no')->nullable();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->unsignedBigInteger('ref_no_to_root_node')->nullable();
-            $table->enum('is_parent',['Yes', 'No']);
+            $table->enum('is_parent', ['Yes', 'No'])->nullable();
             $table->boolean('is_editable')->default(0);
-            $table->string('level_no');
+            $table->string('level_no')->nullable();
             $table->softDeletes();
 
             $table->foreign('fixed_asset_acct_id')->references('id')->on('admin_segments');
