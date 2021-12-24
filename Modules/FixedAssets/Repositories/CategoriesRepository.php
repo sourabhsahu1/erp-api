946992d35dd286a8bfee55ae494cded8c54a35ed
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-12-24 18:14:01 +0530

    Category: parent filter added

--- Modules/FixedAssets/Repositories/CategoriesRepository.php
+++ Modules/FixedAssets/Repositories/CategoriesRepository.php
@@ -9,4 +9,15 @@ class CategoriesRepository extends EloquentBaseRepository
 {
     public $model = FxaCategory::class;
 
+    public function getAll($params = [], $query = null)
+    {
+        if (!$query) {
+            $query = FxaCategory::query();
+        }
+        if (isset($params['inputs']['is_parent'])) {
+            $query->whereNull('parent_id');
+        }
+        return parent::getAll($params, $query);
+    }
+
 }
