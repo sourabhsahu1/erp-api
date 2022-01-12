0fd25d2a22e871ebb6958397743fe9bf02c58592
Manoj Singh Rawat <manojrawat1502@gmail.com>
2022-01-12 21:42:10 +0530

    Fixed Asset: deployments migration added

--- Modules/FixedAssets/Database/Migrations/2021_10_25_070006_fxa_deployments.php
+++ Modules/FixedAssets/Database/Migrations/2021_10_25_070006_fxa_deployments.php
@@ -15,9 +15,21 @@ class FxaDeployments extends Migration
     {
         Schema::create('fxa_deployments', function (Blueprint $table) {
             $table->bigIncrements('id');
-
+            $table->unsignedBigInteger('fxa_assets_id');
+            $table->unsignedBigInteger('custodian_id');
+            $table->date('value_date');
+            $table->unsignedInteger('admin_segment_id');
+            $table->unsignedBigInteger('location_id');
+            $table->string('remark')->nullable();
+            $table->unsignedBigInteger('created_by_id');
             $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('fxa_assets_id')->references('id')->on('fxa_assets');
+            $table->foreign('admin_segment_id')->references('id')->on('admin_segments');
+            $table->foreign('location_id')->references('id')->on('hr_work_locations');
+            $table->foreign('custodian_id')->references('id')->on('users');
+            $table->foreign('created_by_id')->references('id')->on('users');
         });
     }
 
