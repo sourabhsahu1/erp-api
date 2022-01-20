b30dfdca87a2fbaf95037425d5cbccfd7c7070d8
Manoj Singh Rawat <manojrawat1502@gmail.com>
2022-01-21 01:15:12 +0530

    Redeployment

--- Modules/FixedAssets/Http/Controllers/FixedAssetDeploymentsController.php
+++ Modules/FixedAssets/Http/Controllers/FixedAssetDeploymentsController.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Luezoid\Laravelcore\Jobs\BaseJob;
 use Modules\FixedAssets\Repositories\FxaDeploymentRepository;
 
 class FixedAssetDeploymentsController extends BaseController
 {
     protected $repository = FxaDeploymentRepository::class;
     protected $indexWith = ['admin_segment', 'work_location', 'custodian'];
+    protected $storeJobMethod = "create";
+    protected $createJob = BaseJob::class;
 }
