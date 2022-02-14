c386c655e4e06bef29e07850bb5b8f03cdf23be5
Manoj Singh Rawat <manojrawat1502@gmail.com>
2022-02-14 23:13:09 +0530

    Fixed Asset: populate sub category data

--- Modules/FixedAssets/Http/Controllers/FxaCategoriesController.php
+++ Modules/FixedAssets/Http/Controllers/FxaCategoriesController.php
@@ -19,5 +19,5 @@ class FxaCategoriesController extends BaseController
     protected $deleteJobMethod = "delete";
     protected $storeRequest = Create::class;
     protected $updateRequest = Update::class;
-    protected $indexWith = ['sub_categories', 'fixed_asset_acct', 'accum_depr_acct', 'depr_exps_acct'];
+    protected $indexWith = ['fixed_asset_acct', 'accum_depr_acct', 'depr_exps_acct', 'sub_categories', 'sub_categories.fixed_asset_acct', 'sub_categories.accum_depr_acct', 'sub_categories.depr_exps_acct'];
 }
