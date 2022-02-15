71724e38f67056ac86d6469cfffdabc217a498dc
Saurabh <saurabh.sahu@luezoid.com>
2022-02-15 17:25:37 +0530

    fix

--- Modules/FixedAssets/Repositories/FxaDepreciationDetailRepository.php
+++ Modules/FixedAssets/Repositories/FxaDepreciationDetailRepository.php
@@ -40,6 +40,9 @@ class FxaDepreciationDetailRepository extends EloquentBaseRepository
                 foreach ($category->fxa_assets as $asset) {
 
                     if (($asset->acquisition_cost_deprecated < (int)$this->getDepricatedAmount($category, $asset))){
+                        if ((int)$this->getDepricatedAmount($category, $asset) < 0){
+                            continue;
+                        }
                         // add salvage value when depriciation is overs
                         if ($asset->is_depreciation_over == false){
                             $asset->acquisition_cost_deprecated = $asset->acquisition_cost_deprecated + $asset->salvage_value;
